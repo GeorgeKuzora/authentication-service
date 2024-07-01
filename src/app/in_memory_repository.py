aa7ba5db54d2416b939ac4dae9261083c58bc56a
GeorgiyKuzora <rafale87@gmail.com>
2024-07-01 13:15:38 +0300

    Создает метод create_user в InMemoryRepository

--- src/app/in_memory_repository.py
+++ src/app/in_memory_repository.py
@@ -24,3 +24,25 @@ class InMemoryRepository:
         self.users_count: int = 0
         self.tokens: list[Token] = []
         self.tokens_count: int = 0
+
+    def create_user(self, user: User) -> User:
+        """
+        Создает пользователя в базе данных.
+
+        Создает, созраняет в базе данных
+        и возвращает индексированную запись о пользователе.
+
+        Args:
+            user: User - неидексированная запись о пользователе.
+
+        Returns:
+            User - индексированная запись о пользователе.
+        """
+        indexed_user = User(
+            username=user.username,
+            password_hash=user.password_hash,
+            user_id=self.users_count,
+        )
+        self.users_count += 1
+        logger.info(f'Добавил пользователя {indexed_user} в хранилище')
+        return indexed_user
