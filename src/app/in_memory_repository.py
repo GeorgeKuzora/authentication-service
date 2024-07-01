97b9749c4acc689b5046f2093251c5f469aedeb7
GeorgiyKuzora <rafale87@gmail.com>
2024-07-01 12:55:36 +0300

    Создает имплементацию хранилища данных InMemoryRepository

---
+++ src/app/in_memory_repository.py
@@ -0,0 +1,22 @@
+from app.service import User, Token
+
+
+class InMemoryRepository:
+    """
+    Имплементация хранилища данных в оперативной памяти.
+
+    Сохраняет данные только на время работы программы.
+    Для хранения данных использует списки python.
+
+    Attributes:
+        users: list[User] - созданные пользователи.
+        users_count: int - счетчик созданных пользователей.
+        tokens: list[Token] - созданные токены.
+        tokens_count: int - счетчик созданных токенов.
+    """
+
+    def __init__(self) -> None:
+        self.users: list[User] = []
+        self.users_count: int = 0
+        self.tokens: list[Token] = []
+        self.tokens_count: int = 0
