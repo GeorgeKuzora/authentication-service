e3348b3813aba5f5554b4b4cb03603e74b5360ed
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 19:44:55 +0300

    Добавляет класс TestCreateToken в юнит тесты приватных методов

--- src/tests/unit/test_service_priv_func.py
+++ src/tests/unit/test_service_priv_func.py
@@ -46,3 +46,49 @@ class TestCreateUser:
 
         with pytest.raises(RepositoryError):
             service._create_user(user)  # noqa
+
+
+class TestCreateToken:
+    """Тестирует метод _create_token."""
+
+    @pytest.mark.parametrize(
+        'user, expected_token', (
+            pytest.param(
+                user_list[0], token_list[0], id='user 1',
+            ),
+            pytest.param(
+                user_list[1], token_list[1], id='user 2',
+            ),
+        ),
+    )
+    def test_create_token(
+        self, user: User, expected_token: Token, service: AuthService,
+    ):
+        """Тестирует позитивные сценарии."""
+        service.repository.create_token.return_value = expected_token
+
+        response = service._create_token(user)  # noqa
+
+        assert response.subject.username == expected_token.subject.username
+        assert response.subject.user_id == expected_token.subject.user_id
+        assert response.subject.password_hash == expected_token.subject.password_hash  # noqa
+        assert response.issued_at == expected_token.issued_at
+        assert response.encoded_token == expected_token.encoded_token
+
+    @pytest.mark.parametrize(
+        'user, side_effect', (
+            pytest.param(
+                user_list[1],
+                raise_repository_error,
+                id='error in repository',
+            ),
+        ),
+    )
+    def test_create_token_raises(
+        self, user: User, side_effect, service: AuthService,
+    ):
+        """Тестирует негативный сценарий."""
+        service.repository.create_token.side_effect = side_effect
+
+        with pytest.raises(RepositoryError):
+            service._create_token(user)  # noqa
