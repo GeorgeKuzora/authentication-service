e0419b4746c34053845db54a1e4f0651e5a37b05
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 19:41:15 +0300

    Добавляет фикстуру service в conftest.py

--- src/tests/unit/conftest.py
+++ src/tests/unit/conftest.py
@@ -15,3 +15,18 @@ token_list = [
     Token(user_list[0], issued_at, encoded_token, 1),
     Token(user_list[1], issued_at, encoded_token, 2),
 ]
+
+
+@pytest.fixture
+def service():
+    """
+    Фикстура создает экземляр сервиса.
+
+    Атрибуты сервиса repository, config являются mock объектами.
+    """
+    repository = MagicMock()
+    config = MagicMock()
+    config.token_algorithm = 'HS256'
+    config.secret_key = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b89e8d3e7'  # noqa
+
+    return AuthService(repository=repository, config=config)
