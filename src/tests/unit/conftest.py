c76508cf1922d5f97d29c81f3c0764923b551284
GeorgiyKuzora <rafale87@gmail.com>
2024-07-07 19:40:42 +0300

    Добавляет тестовые данные в conftest.py

--- src/tests/unit/conftest.py
+++ src/tests/unit/conftest.py
@@ -4,3 +4,14 @@ from unittest.mock import MagicMock
 import pytest
 
 from app.service import AuthService, RepositoryError, Token, User
+
+issued_at = datetime.now()
+encoded_token = 'sdfa.asfsd.safd'
+user_list = [
+    User('peter', '13rasf', 1),
+    User('max', 'sdfad', 2),
+]
+token_list = [
+    Token(user_list[0], issued_at, encoded_token, 1),
+    Token(user_list[1], issued_at, encoded_token, 2),
+]
