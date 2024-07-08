dd90273a8743db9d3dd3bf83582aea8bfc33c9dd
GeorgiyKuzora <rafale87@gmail.com>
2024-07-08 12:52:44 +0300

    Добавляет класс TestRegister в интеграционные тесты

--- src/tests/integration/test_service_in_mem_repo.py
+++ src/tests/integration/test_service_in_mem_repo.py
@@ -21,3 +21,37 @@ usenames_and_passwords = [
     {'username': 'george', 'password': 'password_1'},
     {'username': 'peter', 'password': 'password_2'},
 ]
+
+
+class TestRegister:
+    """Тестирует метод register."""
+
+    @pytest.mark.parametrize(
+        'username, password', (
+            pytest.param(
+                usenames_and_passwords[0]['username'],
+                usenames_and_passwords[0]['password'],
+                id='valid user 1',
+            ),
+            pytest.param(
+                usenames_and_passwords[1]['username'],
+                usenames_and_passwords[1]['password'],
+                id='valid user 2',
+            ),
+        ),
+    )
+    def test_register(
+        self, username, password, service: AuthService,  # noqa
+    ):
+        """Тестирует метод register."""
+        token = service.register(username, password)
+
+        if token is not None:
+            assert token.subject.username == username
+            assert token.encoded_token
+            assert token.issued_at
+            assert service._verify_password(  # noqa
+                password, token.subject.password_hash,
+            )
+        else:
+            raise AssertionError()
