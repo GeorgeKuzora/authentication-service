d9969d5b0f14c97900587a0ac295088022071046
GeorgiyKuzora <rafale87@gmail.com>
2024-08-04 14:46:15 +0000

    Добавляет хэндлеры healthcheck

--- src/app/service.py
+++ src/app/service.py
@@ -3,11 +3,13 @@ import logging
 from fastapi import FastAPI
 
 from app.api.handlers import router
+from app.api.healthz.handlers import healthz_router
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(router)
+app.include_router(healthz_router)
 
 
 if __name__ == '__main__':
