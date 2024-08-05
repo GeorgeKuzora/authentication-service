a1d342552b9f61d491fbcba196e60b3d88cb94c6
GeorgiyKuzora <rafale87@gmail.com>
2024-08-05 13:18:31 +0000

    Рефакторит код хэндлеров

--- src/app/api/handlers.py
+++ src/app/api/handlers.py
@@ -14,7 +14,7 @@ from pydantic import ValidationError
 
 from app.core.authentication import AuthService
 from app.core.config import get_auth_config
-from app.core.errors import AuthorizationError, NotFoundError
+from app.core.errors import AuthorizationError, NotFoundError, ServerError
 from app.core.models import Token, UserCredentials
 from app.external.in_memory_repository import InMemoryRepository
 from app.external.kafka import KafkaQueue
@@ -36,19 +36,22 @@ def get_service() -> AuthService:
     )
 
 
+service = get_service()
+
+
+@router.post('/login')
 async def authenticate(
     user_creds: UserCredentials,
     authorization: Annotated[str, Header()],
 ) -> Token:
     """Хэндлер аутентификации пользователя."""
-    service = get_service()
     task = asyncio.create_task(service.authenticate(user_creds, authorization))
     try:
         return await task
     except NotFoundError as not_found_err:
         logger.info(f'{user_creds.username} not found')
         raise HTTPException(
-            status_code=status.HTTP_404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail=f'{user_creds.username} not found',
         ) from not_found_err
     except AuthorizationError as auth_err:
@@ -57,17 +60,16 @@ async def authenticate(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f'{user_creds.username} unauthorized',
         ) from auth_err
-    except Exception as err:
-        logger.info('server error in /authenticate')
+    except ServerError as err:
+        logger.info('server error in /login')
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
         ) from err
 
 
 @router.post('/register')
 async def register(user_creds: UserCredentials) -> Token:
     """Хэндлер регистрации пользователя."""
-    service = get_service()
     task = asyncio.create_task(service.register(user_creds))
     try:
         return await task
@@ -88,24 +90,23 @@ async def check_token(
     authorization: Annotated[str, Header()],
 ) -> dict[str, str]:
     """Хэндлер валидации токена пользователя."""
-    service = get_service()
     task = asyncio.create_task(service.check_token(authorization))
     try:
         return await task  # type: ignore
     except NotFoundError as not_found_err:
         logger.info('token not found error in /check_token')
         raise HTTPException(
-            status_code=status.HTTP_404,
+            status_code=status.HTTP_404_NOT_FOUND,
         ) from not_found_err
     except AuthorizationError as auth_err:
         logger.info('authorisation error in /check_token')
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
         ) from auth_err
-    except Exception as err:
+    except ServerError as err:
         logger.error('server error in /check_token')
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
         ) from err
 
 
@@ -116,7 +117,6 @@ async def verify(
     background_tasks: BackgroundTasks,
 ) -> dict[str, str]:
     """Верифицирует пользователя."""
-    service = get_service()
     background_tasks.add_task(
         service.verify, user_creds=user_creds, image=image,
     )
