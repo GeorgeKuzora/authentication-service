aa6754706fe4ca2025e2a6c2c6b25d56a44388c6
GeorgiyKuzora <rafale87@gmail.com>
2024-08-08 10:16:44 +0000

    Добавляет конфигурацию приложения и kafka

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,8 +1,12 @@
 import logging
 import os
+from functools import lru_cache
 from pathlib import Path
+from typing import Self
 
 import dotenv
+import yaml
+from pydantic_settings import BaseSettings
 
 from app.core.errors import ConfigError
 
@@ -38,10 +42,10 @@ class AuthConfigAccessData:
             raise ConfigError(detail='config file path not found')
         elif not self._is_valid_path(self.jwt_secrets_path):
             logger.critical(
-                f'config file {self.jwt_secrets_path} not found',
+                f'authconfig file {self.jwt_secrets_path} not found',
             )
             raise ConfigError(
-                detail=f'config file {self.jwt_secrets_path} not found',
+                detail=f'authconfig file {self.jwt_secrets_path} not found',
             )
 
 
@@ -111,3 +115,57 @@ def get_auth_config() -> AuthConfig:
     except ConfigError as config_error:
         logger.critical('auth config setting failed')
         raise ConfigError(detail='auth config setting failed') from config_error
+
+
+class KafkaSettings(BaseSettings):
+    """Конфигурация kafka producer."""
+
+    host: str
+    port: int
+    file_encoding: str = 'utf-8'
+    file_compression_quality: int = 1
+
+    @property
+    def instance(self) -> str:
+        """
+        Свойство для получения адреса kafka.
+
+        :return: Адрес kafka
+        :rtype: str
+        :raises ConfigError: При ошибке конфигурации сервиса
+        """
+        if not self.host or self.port:
+            raise ConfigError(detail='kafka config setting failed')
+        return f'{self.host}:{self.port}'
+
+
+class Settings(BaseSettings):
+    """Конфигурация приложения."""
+
+    kafka: KafkaSettings
+
+    @classmethod
+    def from_yaml(cls, config_path) -> Self:
+        """Создает объект класса из файла yaml."""
+        if not cls._is_valid_path(config_path):
+            logger.critical(
+                f'config file is missing on path {config_path}',
+            )
+            raise ConfigError(
+                detail=f'config file is missing on path {config_path}',
+            )
+        settings = yaml.safe_load(Path(config_path).read_text())
+        return cls(**settings)
+
+    @classmethod
+    def _is_valid_path(cls, path: str) -> bool:
+        passlib_path = Path(path)
+        return passlib_path.is_file()
+
+
+@lru_cache
+def get_settings() -> Settings:
+    """Создает конфигурацию сервиса."""
+    config_path_env_var = 'CONFIG_PATH'
+    config_file = os.getenv(config_path_env_var)
+    return Settings.from_yaml(config_file)
